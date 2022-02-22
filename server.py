f4ec98114b5581353817bfc36bfcad3290f8020a
deepthisilcontechlabs <100143496+deepthisilcontechlabs@users.noreply.github.com>
2022-02-22 06:50:49 -0500

    Update server.py

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ def hello_world(request):
     name = os.environ.get('NAME')
     if name == None or len(name) == 0:
         name = "world"
-    message = "GoodMorning Happy girl, " + name + "!\n"
+    message = "GoodMorning Happy , " + name + "!\n"
     return Response(message)
 
 if __name__ == '__main__':
